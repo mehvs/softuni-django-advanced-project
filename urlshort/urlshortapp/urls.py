3b8394f4f242038a7c043864501d511a4adc0661
mehvs <57858736+mehvs@users.noreply.github.com>
2024-04-08 22:28:48 +0300

    added redirect to original view

--- urlshort/urlshortapp/urls.py
+++ urlshort/urlshortapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Index, CreateShortUrl
+from .views import Index, CreateShortUrl, redirect_to_original
 
 urlpatterns = [
 
     path('', CreateShortUrl.as_view(), name='shortener'),
+    path('<str:short_code>/', redirect_to_original, name='redirect'),
 
 ]
--- urlshort/urlshortapp/views.py
+++ urlshort/urlshortapp/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView, CreateView
-from .forms import ShortenerForm
 from django.urls import reverse_lazy
 from .models import Url
 
 # Create your views here.
 
 from django.http import HttpResponse
+
+
+def redirect_to_original(request, short_code):
+    url = Url.objects.get(short_code=short_code)
+    return redirect(url.original_url)
 
 
 class Index(TemplateView):
@@ -20,5 +24,6 @@ class CreateShortUrl(CreateView):
     success_url = reverse_lazy('shortener')
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         form.instance.short_code = Url.short_code
         return super(CreateShortUrl, self).form_valid(form)
