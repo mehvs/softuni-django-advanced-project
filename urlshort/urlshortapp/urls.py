56925d165bf5bfd0c538a56cd2602e6ce3ec53c1
mehvs <57858736+mehvs@users.noreply.github.com>
2024-04-09 19:28:36 +0300

    Created logout system

--- urlshort/urlshortapp/urls.py
+++ urlshort/urlshortapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import Index, CreateShortUrl, redirect_to_original, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', CreateShortUrl.as_view(), name='shortener'),
     path('<str:short_code>/', redirect_to_original, name='redirect'),
 
